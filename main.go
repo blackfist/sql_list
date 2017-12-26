ac96b802ebce26a925eb7478c2b85d539345e53e
Kevin Thompson <kevin.david.thompson@gmail.com>
2017-12-26 16:44:26 -0600

    Create main.go

---
+++ main.go
@@ -0,0 +1,34 @@
+// Package main is a program that takes a list of things
+// from stdin and downcases all the items and creates a
+// list suitable for pasting into an sql statement
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	info, _ := os.Stdin.Stat()
+	if (info.Mode() & os.ModeNamedPipe != 0) {  
+		values := make([]string, 0)
+		reader := bufio.NewScanner(os.Stdin)
+		fmt.Print("(")
+		for reader.Scan() {
+			var buffer bytes.Buffer
+			buffer.WriteString("'")
+			buffer.WriteString(strings.ToLower(reader.Text()))
+			buffer.WriteString("'")
+			values = append(values, buffer.String())
+		}
+		
+		fmt.Print(strings.Join(values, "."))
+		fmt.Print(")")
+	} else {
+		fmt.Println("you gotta pipe in some shit, fool")
+	}
+	
+}
